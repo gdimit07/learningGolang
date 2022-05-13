cb302b763549c161346f69f0b1cda66db515494c
giorgosd <giorgosd@amdocs.com>
2022-05-13 17:49:06 +0300

    save and read from file, shuffle deck, pending tests

--- main.go
+++ main.go
@@ -1,29 +1,12 @@
 package main
 
-import "fmt"
-
 // func newCard() string {
 // 	return "Five of Diamonds"
 // }
 
 func main() {
 
-	//cards := []string{newCard(), newCard(), "Ace of Diamonds"}
-	//cards := deck{newCard(), "Ace of Diamonds"}
-	//cards = append(cards, "Six of hearts")
-
-	// for i, card := range cards {
-	// 	fmt.Println(i, card)
-	// }
-
-	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-
-	remainingCards.print()
-
-	fmt.Println(cards.toString())
-
+	cards := newDeckFromFile("my_cards")
+	cards.shuffle()
+	cards.print()
 }
